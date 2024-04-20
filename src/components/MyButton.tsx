f52a25b4c764cb66840aa785ebc565bf7795b1e6
BingBong <bingbong9981@gmail.com>
2024-04-21 03:14:34 +0900

    [SB-12] Feat: 페이지 완성

--- src/components/MyButton.tsx
+++ src/components/MyButton.tsx
@@ -12,8 +12,8 @@ const MyButton = () => {
         aria-hidden="true"
       >
         <path
-          clip-rule="evenodd"
-          fill-rule="evenodd"
+          clipRule="evenodd"
+          fillRule="evenodd"
           d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
         ></path>
       </svg>
