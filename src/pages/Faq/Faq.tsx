88f013d9938e3b6da4f1795d01c867562b7b15bb
Seoyoung Yoon <syyoon0118@gmail.com>
2024-04-22 15:05:04 +0900

    [SB-21] Feat: Fnq Page

--- src/pages/Faq/Faq.tsx
+++ src/pages/Faq/Faq.tsx
@@ -1,12 +1,16 @@
+
 import MyButton from "../../components/MyButton";
 import MyHeader from "../../components/MyHeader";
+import FaqViewer from "./FaqViewer";
 
 function Faq() {
   return (
     <div className="Faq">
       <MyHeader headerText="자주하는 질문" leftChild={<MyButton />} />
+      <FaqViewer />
     </div>
   );
 }
 
 export default Faq;
+
