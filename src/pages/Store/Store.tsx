8a2211660a2b15c8f6f7c7b9eb2df88f4fc7f0d1
BingBong <bingbong9981@gmail.com>
2024-05-21 19:23:58 +0900

    [SB3-25] Feat: 헤더&달 생성

--- src/pages/Store/Store.tsx
+++ src/pages/Store/Store.tsx
@@ -1,10 +1,20 @@
+import MyButton from "../../components/MyButton/MyButton";
+import MyHeader from "../../components/MyHeader/MyHeader";
 import "./Store.css";
 
 function Store() {
+  const user_dal = 24;
+
   return (
-    <div>
-      <h1> 상점 페이지입니다</h1>
-      <p>작업중</p>
+    <div className="Store">
+      <MyHeader headerText="상점" leftChild={<MyButton />} />
+      <div className="user_dal">
+        <img src="/src/assets/images/moon.png" />
+        <p>{user_dal} Dal</p>
+      </div>
+      <div className="">
+        <p>상점...</p>
+      </div>
     </div>
   );
 }
