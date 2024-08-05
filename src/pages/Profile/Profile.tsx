6e68ca3cadf9d9b54699af3a2e3570f57c8023be
BingBong <bingbong9981@gmail.com>
2024-08-05 22:42:24 +0900

    Feat: 공개여부 설정 툴팁 추가

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -301,6 +301,27 @@ function Profile() {
           <p className="list_name">
             프로필<br></br>공개여부
           </p>
+          <div className="tooltip">
+            <svg
+              className="tooltip_icon"
+              data-slot="icon"
+              fill="none"
+              strokeWidth="2.5"
+              stroke="#8c8c8c"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z"
+              ></path>
+            </svg>
+            <span className="tooltip_text">
+              공개 데이터:<br></br>이름, 지갑 주소, 보유 NFT 정보
+            </span>
+          </div>
           <div className="type">
             {isEditingType ? (
               <>
