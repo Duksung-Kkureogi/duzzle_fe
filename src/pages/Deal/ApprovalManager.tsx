f389cda495bb35610eb0f6dcb22c52cbe448a090
BingBong <bingbong9981@gmail.com>
2024-10-29 12:34:40 +0900

    Design: 거래 로딩 모달창 크기 조정

--- src/pages/Deal/ApprovalManager.tsx
+++ src/pages/Deal/ApprovalManager.tsx
@@ -117,7 +117,7 @@ const ApprovalManager: React.FC<ApprovalManagerProps> = ({
       bottom: "auto",
       transform: "translate(-50%, -50%)",
       width: "300px",
-      height: "250px",
+      height: "300px",
       borderRadius: "20px",
       justifyContent: "center",
       backgroundColor: "#F69EBB",
